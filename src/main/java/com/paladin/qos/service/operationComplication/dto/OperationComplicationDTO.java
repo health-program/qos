2db2e54ced7a4880acc3184ff91313092bb0bbbd
dannaan <1982911028@qq.com>
2019-08-15 13:40:40 +0800

    手术风险

---
+++ src/main/java/com/paladin/qos/service/operationComplication/dto/OperationComplicationDTO.java
@@ -0,0 +1,76 @@
+package com.paladin.qos.service.operationComplication.dto;
+
+public class OperationComplicationDTO {
+	public String id;
+	  public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String embolism;
+	public String createUserId;
+	
+	  public String getCreateUserId() {
+		return createUserId;
+	}
+	public void setCreateUserId(String createUserId) {
+		this.createUserId = createUserId;
+	}
+	public String pulmonaryEmbolism;
+	  public String venousThrombosis;
+	  public String hematoma;
+	  public String woundDehiscence;
+	  public String suddenDeath;
+	  public String operativeComplications;
+	  public String anaesthesia;
+	public String getEmbolism() {
+		return embolism;
+	}
+	public void setEmbolism(String embolism) {
+		this.embolism = embolism;
+	}
+	public String getPulmonaryEmbolism() {
+		return pulmonaryEmbolism;
+	}
+	public void setPulmonaryEmbolism(String pulmonaryEmbolism) {
+		this.pulmonaryEmbolism = pulmonaryEmbolism;
+	}
+	public String getVenousThrombosis() {
+		return venousThrombosis;
+	}
+	public void setVenousThrombosis(String venousThrombosis) {
+		this.venousThrombosis = venousThrombosis;
+	}
+	public String getHematoma() {
+		return hematoma;
+	}
+	public void setHematoma(String hematoma) {
+		this.hematoma = hematoma;
+	}
+	public String getWoundDehiscence() {
+		return woundDehiscence;
+	}
+	public void setWoundDehiscence(String woundDehiscence) {
+		this.woundDehiscence = woundDehiscence;
+	}
+	public String getSuddenDeath() {
+		return suddenDeath;
+	}
+	public void setSuddenDeath(String suddenDeath) {
+		this.suddenDeath = suddenDeath;
+	}
+	public String getOperativeComplications() {
+		return operativeComplications;
+	}
+	public void setOperativeComplications(String operativeComplications) {
+		this.operativeComplications = operativeComplications;
+	}
+	public String getAnaesthesia() {
+		return anaesthesia;
+	}
+	public void setAnaesthesia(String anaesthesia) {
+		this.anaesthesia = anaesthesia;
+	}
+	  
+}
