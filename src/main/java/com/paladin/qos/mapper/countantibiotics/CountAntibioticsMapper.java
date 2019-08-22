a08dd44e549270603524839c65f4aebeafd985c0
dannaan1 <1982911028@qq.com>
2019-08-22 17:21:44 +0800

    类名变成大写开头

---
+++ src/main/java/com/paladin/qos/mapper/countantibiotics/CountAntibioticsMapper.java
@@ -0,0 +1,16 @@
+package com.paladin.qos.mapper.countantibiotics;
+
+import java.util.List;
+
+import com.paladin.framework.core.configuration.mybatis.CustomMapper;
+import com.paladin.qos.model.countantibiotics.CountAntibiotics;
+import com.paladin.qos.service.countantibiotics.dto.CountAntibioticsDTO;
+import com.paladin.qos.service.countantibiotics.vo.CountAntibioticsVO;
+
+public interface CountAntibioticsMapper extends CustomMapper<CountAntibiotics> {
+
+	public List<CountAntibioticsVO> selecttoAll(CountAntibioticsDTO query);
+
+	public Object queryById(String id);
+
+}
--- src/main/java/com/paladin/qos/service/countantibiotics/CountAntibioticsService.java
+++ src/main/java/com/paladin/qos/service/countantibiotics/CountAntibioticsService.java
@@ -7,7 +7,7 @@ import com.github.pagehelper.Page;
 import com.github.pagehelper.PageHelper;
 import com.paladin.framework.common.PageResult;
 import com.paladin.framework.core.ServiceSupport;
-import com.paladin.qos.mapper.countantibiotics.countAntibioticsMapper;
+import com.paladin.qos.mapper.countantibiotics.CountAntibioticsMapper;
 import com.paladin.qos.model.countantibiotics.CountAntibiotics;
 import com.paladin.qos.service.countantibiotics.dto.CountAntibioticsDTO;
 import com.paladin.qos.service.countantibiotics.vo.CountAntibioticsVO;
@@ -15,7 +15,7 @@ import com.paladin.qos.service.countantibiotics.vo.CountAntibioticsVO;
 @Service
 public class CountAntibioticsService  extends ServiceSupport<CountAntibiotics> {// @Autowired
     @Autowired
-	countAntibioticsMapper CountAntibioticsMapper; 
+	CountAntibioticsMapper CountAntibioticsMapper; 
 	  public PageResult<CountAntibioticsVO> searchFindPage(CountAntibioticsDTO query){
 	       Page<CountAntibioticsVO> page = PageHelper.offsetPage(query.getOffset(), query.getLimit()); 
 	       CountAntibioticsMapper.selecttoAll(query);
