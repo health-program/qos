9df36d2f5e46a46eefc5d94b463ee3f509a442bd
Tonto <823498927@qq.com>
2019-09-17 18:25:24 +0800

    bug fix

--- src/main/java/com/paladin/qos/analysis/impl/fuyou/FuyouDataProcessor.java
+++ src/main/java/com/paladin/qos/analysis/impl/fuyou/FuyouDataProcessor.java
@@ -7,16 +7,20 @@ import com.paladin.qos.analysis.DataConstantContainer.Unit;
 import com.paladin.qos.analysis.DataProcessor;
 import com.paladin.qos.analysis.RateMetadata;
 
-public abstract class FuyouDataProcessor extends DataProcessor{
+public abstract class FuyouDataProcessor extends DataProcessor {
 
-	public String getMappingUnitId(String unitId) {		
+	public String getMappingUnitId(String unitId) {
 		Unit unit = DataConstantContainer.getUnit(unitId);
-		return unit == null ? null : unit.getSource().getFuyouCode();	
-	} 
+		return unit == null ? null : unit.getSource().getFuyouCode();
+	}
 
 	public RateMetadata processByDay(Date startTime, Date endTime, String unitId) {
-		unitId = getMappingUnitId(unitId);
-		return super.processByDay(startTime, endTime, unitId);
+		String mappingUnitId = getMappingUnitId(unitId);
+		RateMetadata data = super.processByDay(startTime, endTime, mappingUnitId);
+		if (data != null) {
+			data.setUnitValue(unitId);
+		}
+		return data;
 	}
-	
+
 }
