bd8aabc9b02e0cf2b921e93ca2675df3b3b475ed
dannaan1 <1982911028@qq.com>
2019-09-26 17:31:09 +0800

    静态页面

---
+++ src/main/resources/static/js/quailtydisplay/paymethods.js
@@ -0,0 +1,82 @@
+$(function(){
+
+    //支付方式对比
+         var arr = {
+           eventIds:'21004'
+         }
+      $.ajax({
+         type : "post",    //请求类型
+        url : "/qos/analysis/data/get/month/instalments",//请求的 URL地址
+        data:arr,
+        success: function (rawData) {
+       var  month21004Data=convertMonthChartData(rawData.result, '21004', true); //
+       var option = {
+        tooltip: {
+                             trigger: 'axis',
+                             axisPointer: {
+                                 type: 'shadow'
+                             }
+                         },
+        xAxis: {
+            type: 'category',
+            data: month21004Data.month,
+            axisLine: {
+                lineStyle: {
+                    color: '#657ca8',
+                    width: 1, //这里是为了突出显示加上的
+                }
+            },
+            axisLabel: {
+                show: true,
+                textStyle: {
+                    color: '#19d1ff'
+                },
+                 interval:0,
+                 rotate:40
+            }
+          },
+        yAxis: {
+            splitLine: {
+                show: false
+            },
+            axisLine: {
+                lineStyle: {
+                    color: '#657ca8',
+                    width: 1, //这里是为了突出显示加上的
+                }
+            },
+            axisLabel: {
+                show: true,
+                textStyle: {
+                    color: '#19d1ff'
+                }
+            }
+        },
+         series:[{
+            data: month21004Data.valuesMap['total'], // total 如果是单位，就改成该单位的id
+            type: 'line',
+            lineStyle: {
+                color: '#f45e23',
+                width: 2, //这里是为了突出显示加上的
+            }
+          }]
+    };
+
+    //初始化echarts实例
+
+
+         var myChart4 = echarts.init(document.getElementById('signingDoctor'));
+         myChart4.setOption(option);
+
+
+     window.addEventListener("resize", function () {
+                         myChart4.resize();
+    });
+
+         }
+    });
+
+
+
+
+})
