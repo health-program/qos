6b950c39d114aa99004833d810e584975ccf180f
luckywcw <luckywcw@163.com>
2019-10-10 19:47:07 +0800

    机构地址信息

---
+++ src/main/java/com/paladin/qos/controller/address/AddressController.java
@@ -0,0 +1,63 @@
+package com.paladin.qos.controller.address;
+
+
+import com.paladin.framework.core.ControllerSupport;
+import com.paladin.framework.web.response.CommonResponse;
+import com.paladin.qos.controller.analysis.AnalysisRequest;
+import com.paladin.qos.model.address.Address;
+import com.paladin.qos.model.familydoctor.FamilyDoctorUnit;
+import com.paladin.qos.service.address.AddressService;
+import com.paladin.qos.service.address.vo.AddressVo;
+import com.paladin.qos.service.analysis.data.DataCountUnit;
+import com.paladin.qos.service.count.vo.CountReferralVO;
+import com.paladin.qos.service.gongwei.vo.ArchivesManagementVO;
+import org.apache.commons.collections.CollectionUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.thymeleaf.util.StringUtils;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+@Controller
+@RequestMapping("/qos/address")
+public class AddressController extends ControllerSupport {
+
+    @Autowired
+    private AddressService addressService;
+
+    @RequestMapping(value = "/search/all", method = {RequestMethod.GET, RequestMethod.POST})
+    @ResponseBody
+    public Object searchAllAddress() {
+        List<Address> addressList = new ArrayList<>();
+        List<AddressVo> addressVoList=new ArrayList<>();
+        addressList=addressService.getAddress(null);
+        for (Address address:addressList){
+            AddressVo addressVo=new AddressVo();
+            addressVo=beanCopy(addressService.get(address.getUnitId()), addressVo);
+            addressVoList.add(addressVo);
+        }
+        return CommonResponse.getSuccessResponse(addressVoList);
+    }
+
+    @RequestMapping(value = "/search/community", method = {RequestMethod.GET, RequestMethod.POST})
+    @ResponseBody
+    public Object searchCommunity() {
+        List<Address> addressList = new ArrayList<>();
+        List<AddressVo> addressVoList=new ArrayList<>();
+        addressList=addressService.getAddress(2);
+        for (Address address:addressList){
+            AddressVo addressVo=new AddressVo();
+            addressVo=beanCopy(addressService.get(address.getUnitId()), addressVo);
+            addressVoList.add(addressVo);
+        }
+        return CommonResponse.getSuccessResponse(addressVoList);
+    }
+
+
+}
---
+++ src/main/java/com/paladin/qos/service/address/AddressService.java
@@ -0,0 +1,23 @@
+package com.paladin.qos.service.address;
+
+import com.paladin.framework.core.ServiceSupport;
+import com.paladin.qos.mapper.address.AddressMapper;
+import com.paladin.qos.model.address.Address;
+import com.paladin.qos.model.count.CountReferral;
+import com.paladin.qos.service.address.vo.AddressVo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class AddressService extends ServiceSupport<Address> {
+
+
+    @Autowired
+    private AddressMapper addressMapper;
+
+    public List<Address> getAddress(Integer type){
+       return addressMapper.getAddressInfo(type);
+    }
+}
