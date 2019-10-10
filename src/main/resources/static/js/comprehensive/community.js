0fa2e354eb86ea33dc755c9e57346c5a0c57df33
dannaan1 <1982911028@qq.com>
2019-10-10 09:28:45 +0800

    静态页面

--- src/main/resources/static/js/comprehensive/community.js
+++ src/main/resources/static/js/comprehensive/community.js
@@ -1,53 +1,9 @@
-
 $(function(){
-
-
-
-    $("#selectID").change(function(){
-       echartses()
-    })
-
-    function convertUnitChartData(data, eventId, isRate) {
-        var edata = data[eventId],
-            max = 0,
-            unit = [],
-            values = [];
-        edata && edata.forEach(function(item) {
-            var r = isRate ? getRateNum(item) : item.count;
-            max = Math.max(r, max);
-            values.push(r);
-            unit.push(item.unitName);
-        });
-        return {
-            max: max,
-            unit: unit,
-            values: values
-        }
-    }
-
-    //指定图标的配置和数据
-    // 21001 综合健康管理服务包签约率（收费）
-       var arr = {
-           eventIds:'21021,21022,21023,21024,21025,21026,21027,21028,21029'
-       }
-
-
-      $.ajax({
+     $.ajax({
        type : "post",    //请求类型
-        url : "/qos/analysis/data/get/unit",//请求的 URL地址
-        data:arr,
-        success: function (rawData) {
-           rawData = rawData.result;
-           dataMap['21021']=convertUnitChartData(rawData, '21021', true)
-           dataMap['21022']=convertUnitChartData(rawData, '21022', true)
-           dataMap['21023']=convertUnitChartData(rawData, '21023', true)
-           dataMap['21024']=convertUnitChartData(rawData, '21024', true)
-           dataMap['21025']=convertUnitChartData(rawData, '21025', true)
-           dataMap['21026']=convertUnitChartData(rawData, '21026', true)
-           dataMap['21027']=convertUnitChartData(rawData, '21027', true)
-           dataMap['21028']=convertUnitChartData(rawData, '21028', true)
-           dataMap['21029']=convertUnitChartData(rawData, '21029', true)
-           echartses()
+        url : "/qos/family/doctor/team/data/count",//请求的 URL地址
+        success: function (res) {
+             $("#home_family_doctor").text(res.result)
 
          }
     });
