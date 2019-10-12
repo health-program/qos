bd873108e98219069f82eadce6ff103753e6e5d6
zhouya12345 <632394831@qq.com>
2019-10-12 13:16:07 +0800

    aaaa
    aaaa

--- src/main/java/com/paladin/qos/service/analysis/AnalysisConstant.java
+++ src/main/java/com/paladin/qos/service/analysis/AnalysisConstant.java
@@ -1,6 +1,16 @@
 package com.paladin.qos.service.analysis;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class AnalysisConstant {
 	/** 特殊单位妇幼 */
 	public final static String SPECIAL_UNIT_FUYOU = "320583810343";
+
+	public final static List<String> SPECIAL_UNITS_FUYOU;
+
+	static {
+		SPECIAL_UNITS_FUYOU = new ArrayList<>();
+		SPECIAL_UNITS_FUYOU.add(SPECIAL_UNIT_FUYOU);
+	}
 }
