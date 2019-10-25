43b15486fd139f3dabe32c5105885a1ce6c16c7f
Huangguochen <896599830@qq.com>
2019-10-25 08:55:26 +0800

    日志管理

--- src/main/java/com/paladin/common/service/syst/dto/SysLoggerLoginQuery.java
+++ src/main/java/com/paladin/common/service/syst/dto/SysLoggerLoginQuery.java
@@ -1,7 +1,20 @@
 package com.paladin.common.service.syst.dto;
 
 import com.paladin.framework.common.OffsetPage;
+import com.paladin.framework.common.QueryCondition;
+import com.paladin.framework.common.QueryType;
 
 public class SysLoggerLoginQuery extends OffsetPage {
 
+    // 登录账号
+    private String account;
+
+    @QueryCondition(type = QueryType.EQUAL)
+    public String getAccount() {
+        return account;
+    }
+
+    public void setAccount(String account) {
+        this.account = account;
+    }
 }
