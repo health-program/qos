55ba480f3f66a4f92cd719a2c9099e843a05f46f
Tonto <823498927@qq.com>
2019-11-05 19:16:07 +0800

    读取医院数据库

---
+++ src/main/java/com/paladin/qos/service/hospital/HospitalDataService.java
@@ -0,0 +1,17 @@
+package com.paladin.qos.service.hospital;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.paladin.data.dynamic.SqlSessionContainer;
+
+@Service
+public class HospitalDataService {
+
+	@Autowired
+	private SqlSessionContainer sqlSessionContainer;
+	
+	
+
+	
+}
